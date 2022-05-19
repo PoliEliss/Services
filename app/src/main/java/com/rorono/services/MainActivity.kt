5ac32feb029186574f591b2b2ec06a1033a7888f
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2022-05-19 21:09:04 +0300

    add notification

--- app/src/main/java/com/rorono/services/MainActivity.kt
+++ app/src/main/java/com/rorono/services/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.rorono.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.NotificationCompat
 import com.rorono.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var id = 0
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
@@ -16,5 +23,34 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newIntent(this))
         }
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notificationChannel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Title")
+            .setContentText("jjjjjj")
+            .build()
+
+
+
+        notificationManager.notify(id++, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME ="channel_name"
+
     }
 }
